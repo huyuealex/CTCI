c9e2b4ae75521f80636a277891752630b204861d
huyuealex <huyuealex@sina.com>
2014-02-22 23:06:52 -0500

    new Xcode project

---
+++ CodeInterview/CodeInterview/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CodeInterview
+//
+//  Created by huyuealex on 2/22/14.
+//  Copyright (c) 2014 huyuealex. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
